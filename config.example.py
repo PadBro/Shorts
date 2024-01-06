724159bbfa3b30534db33a3eee66c77ef6c08b3c
PadBro <padmaster2012@googlemail.com>
2024-01-07 00:00:04 +0100

    add missing config

--- config.example.py
+++ config.example.py
@@ -4,6 +4,7 @@ subreddit = "AmItheAsshole"
 max_videos = 1
 part_buffer_in_seconds = 2
 video_end_buffer_in_seconds = 1
+max_part_length_in_seconds = 57 # 2 seconds for the part buffer 1 second for youtube
 max_video_length_in_seconds = (max_videos * 60) - (max_videos * part_buffer_in_seconds) - video_end_buffer_in_seconds
 
 discord_token = ""
